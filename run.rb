e27d1bdeb662e85eca59a64ea703f7c65cb55867
Guoliang Cao <gcao99@gmail.com>
2016-05-04 22:47:31 -0400

    Run command in selected pane

--- run.rb
+++ run.rb
@@ -2,23 +2,32 @@
 
 require './tmux'
 
+if ARGV.empty?
+  puts <<-USAGE
+Usage:
+  # Run 'ls' in active pane of first window
+  ruby #{__FILE__} 1 ls
+  # Run 'ls -l' in first pane of first window and make the first pane active
+  ruby #{__FILE__} 1.1 ls -l
+  # Run 'ls' in active pane of active window
+  ruby #{__FILE__} ls
+  USAGE
+  exit 0
+end
+
+if ARGV[0] =~ /^(\d)(\.(\d))?$/
+  ARGV.shift
+  window, pane = $1, $3
+  cmd = "#{TMUX} select-window -t #{window}"
+  cmd += "; #{TMUX} select-pane -t:.#{pane.to_i - 1}" if pane
+  puts cmd
+  system cmd
+
+  target = "-t:#{window}"
+  target += ".#{pane.to_i - 1}" if pane
+end
+
 if ARGV.length > 0
-  if ARGV[0] =~ /^(\d)(\.(\d))?$/
-    ARGV.shift
-    window, pane, rest = $1, $3, $4
-    cmd = "#{TMUX} select-window -t #{window}"
-    cmd += "; #{TMUX} select-pane -t:.#{pane.to_i - 1}" if pane
-    puts cmd
-    system cmd
-
-    target = "-t:#{window}"
-    target += ".#{pane}" if pane
-  end
-
-  if ARGV.length > 0
-    cmd = "#{TMUX} send-keys #{target} \"#{ARGV.join(' ')}\" C-m"
-    puts cmd
-    system cmd
-  end
+  system "#{TMUX} send-keys #{target} \"#{ARGV.join(' ')}\" C-m"
 end
 
