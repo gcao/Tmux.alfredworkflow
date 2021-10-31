7a32cb62f03437f9fbeba4d694f6e2e61e34cc97
Guoliang Cao <gcao99@gmail.com>
2021-10-31 16:47:15 -0400

    Working on tmux.gene

--- src/test.rs
+++
@@ -1,9 +0,0 @@
-extern crate alfred_tmux_workflow;
-
-use alfred_tmux_workflow::tmux::Tmux;
-
-fn main() {
-    let mut tmux = Tmux::new();
-    let window = tmux.windows.get_mut(0).unwrap();
-    window.load_panes();
-}
